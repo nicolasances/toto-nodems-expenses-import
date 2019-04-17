04f840f25f90136bd6370b7b3b1c30a3c8ac97e9
nicolas <nicolas.matteazzi@gmail.com>
2019-04-17 19:03:13 +0200

    log

--- dlg/SaveUpload.js
+++ dlg/SaveUpload.js
@@ -10,7 +10,11 @@ exports.do = (upload) => {
 
   return new Promise((success, failure) => {
 
+    console.log(config);
+
     return MongoClient.connect(config.mongoUrl, function(err, db) {
+
+      console.log(db);
 
       db.db(config.dbName).collection(config.collections.uploads).insertOne(upload, function(err, res) {
 
