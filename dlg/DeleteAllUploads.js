eb2c5b2b73a4e3064e287f5a420e46e94712ac99
nicolas <nicolas.matteazzi@gmail.com>
2019-05-12 18:19:18 +0200

    delete only for sspecific user

--- dlg/DeleteAllUploads.js
+++ dlg/DeleteAllUploads.js
@@ -6,13 +6,16 @@ var MongoClient = mongo.MongoClient;
 /**
  * Saves the upload to DB
  */
-exports.do = function(upload) {
+exports.do = function(req) {
 
   return new Promise(function(success, failure) {
 
+    // User check
+    if (!req.query.user) {failure({code: 400, message: 'The user is mandatory (query param) to upload expenses.'}); return; }
+
     return MongoClient.connect(config.mongoUrl, function(err, db) {
 
-      db.db(config.dbName).collection(config.collections.uploads).deleteMany().then(() => {
+      db.db(config.dbName).collection(config.collections.uploads).deleteMany({user: req.query.user}).then(() => {
 
         db.close();
 
