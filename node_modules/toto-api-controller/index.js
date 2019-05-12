4c5f21c64453de09a8682c1ec992810e8a5e669b
nicolas <nicolas.matteazzi@gmail.com>
2019-05-12 17:43:02 +0200

    new api controller

--- node_modules/toto-api-controller/index.js
+++ node_modules/toto-api-controller/index.js
@@ -131,7 +131,7 @@ class TotoAPIController {
           // When done, call the delegate
           fstream.on('close', () => {
 
-            delegate.do({query: req.query, params: req.params, headers: req.headers, body: {filepath: dir + '/' + filename}}).then((data) => {
+            delegate.do({query: req.query, params: req.params, headers: req.headers, body: {filepath: dir + '/' + filename, ...req.body}}).then((data) => {
               // Success
               res.status(200).type('application/json').send(data);
 
